f71ba4a68b3ff384ecb11627e6b5331eb0e4ddc7
fjavaler <conscientiadivinus@gmail.com>
2017-03-18 09:11:36 -0600

    uploading project to github

---
+++ assignment2.php
@@ -0,0 +1,190 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <!-- Latest compiled JavaScript -->
+        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js">
+        </script>
+        <!-- jQuery library -->
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js">
+        </script>
+        <title>Assignment 2</title>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link href="https://fonts.googleapis.com/css?family=Noto+Sans" rel="stylesheet">
+        <link rel="stylesheet" type = "text/css" href="css/normalize.css" >
+        <link rel="stylesheet" type="text/css" href="css/assignment2Styles.css">
+        <link rel="stylesheet" href="css/bootstrap.min.css">
+        <link rel="stylesheet" href="https://www.w3schools.com/lib/w3.css">
+    </head>
+    <body>
+        <!-- !PAGE CONTENT! -->
+        <div class="w3-content" style="max-width:100%">
+
+            <!-- Header -->
+            <header class="w3-container w3-xlarge w3-padding-18">
+                <a href="/myWebPage.php" class="w3-left w3-btn w3-blue-grey">Home</a>
+
+                <a href="#about" class="w3-right w3-btn w3-blue-grey">About</a>
+                <a href="/exercises.php" class="w3-right w3-btn w3-blue-grey">Exercises</a>
+                <a href="/e-commerce.php" class="w3-right w3-btn w3-blue-grey">E-commerce</a>
+                <a href="/assignment3.php" class="w3-right w3-btn w3-blue-grey">Assignment 3</a>
+                <a href="/assignment2.php" class="w3-right w3-btn w3-blue-grey">Assignment 2</a>
+                <a href="/assignment1.php" class="w3-right w3-btn w3-blue-grey">Assignment 1</a>
+            </header>
+
+            <!-- Photo -->
+            <div class="w3-row">
+                <div class="w3-quarter w3-container"></div>
+                <div class="w3-quarter w3-container">
+                    <img src="/images/friendsAndFamily.png" id="friendsPic" class="img-rounded" alt="Friends and Family">
+                </div>
+                <div class="w3-quarter w3-container"></div>
+                <div class="w3-quarter w3-container"></div>
+            </div>
+        </div>
+
+        <!-- Footer and form -->
+        <div class="container" id="footerContainer">
+            <footer class="w3-padding-64 w3-center" id="friendsAndFamily">
+                <h1 style="color: #404040"><b>Friends and Family</b><br><br></h1>
+                <p>Please enter the first and last name, or phone number, of the person whose contact information you are searching for.</p>
+                <p>Or feel free to create an account! Just fill out all of the fields below.</p>
+                <br><br>
+                <div class="container col-lg-3" id="tan">
+                    <form style="margin:auto;width:90%" method="post" role="form" action="assignment2Return.php">
+                        <div class="container">
+                            <div class="form-group">
+                                <label for="first">First Name:</label>
+                                <input name="first" type="text" class="form-control">
+                            </div>
+                            <div class="form-group">
+                                <label for="last">Last Name:</label>
+                                <input name="last" type="text" class="form-control">
+                            </div>
+                            <div class="form-group">
+                                <label for="phone">Phone Number:</label>
+                                <input name="phone" type="text" class="form-control"  placeholder="(555) 555-5555">
+                            </div>
+                            <div class="form-group">
+                                <label for="address">Address:</label>
+                                <input name="address" type="text" class="form-control">
+                            </div>
+                        </div>
+
+                        <div class="container">
+                            <div class="form-group">
+                                <label for="city">City:</label>
+                                <input name="city" type="text" class="form-control">
+                            </div>
+                            <div class="form-group">
+                                <label for="state">State:</label>
+                                <br>
+                                <select name="state">
+                                    <option value="AL">Alabama</option>
+                                    <option value="AK">Alaska</option>
+                                    <option value="AZ">Arizona</option>
+                                    <option value="AR">Arkansas</option>
+                                    <option value="CA">California</option>
+                                    <option value="CO">Colorado</option>
+                                    <option value="CT">Connecticut</option>
+                                    <option value="DE">Delaware</option>
+                                    <option value="DC">District Of Columbia</option>
+                                    <option value="FL">Florida</option>
+                                    <option value="GA">Georgia</option>
+                                    <option value="HI">Hawaii</option>
+                                    <option value="ID">Idaho</option>
+                                    <option value="IL">Illinois</option>
+                                    <option value="IN">Indiana</option>
+                                    <option value="IA">Iowa</option>
+                                    <option value="KS">Kansas</option>
+                                    <option value="KY">Kentucky</option>
+                                    <option value="LA">Louisiana</option>
+                                    <option value="ME">Maine</option>
+                                    <option value="MD">Maryland</option>
+                                    <option value="MA">Massachusetts</option>
+                                    <option value="MI">Michigan</option>
+                                    <option value="MN">Minnesota</option>
+                                    <option value="MS">Mississippi</option>
+                                    <option value="MO">Missouri</option>
+                                    <option value="MT">Montana</option>
+                                    <option value="NE">Nebraska</option>
+                                    <option value="NV">Nevada</option>
+                                    <option value="NH">New Hampshire</option>
+                                    <option value="NJ">New Jersey</option>
+                                    <option value="NM">New Mexico</option>
+                                    <option value="NY">New York</option>
+                                    <option value="NC">North Carolina</option>
+                                    <option value="ND">North Dakota</option>
+                                    <option value="OH">Ohio</option>
+                                    <option value="OK">Oklahoma</option>
+                                    <option value="OR">Oregon</option>
+                                    <option value="PA">Pennsylvania</option>
+                                    <option value="RI">Rhode Island</option>
+                                    <option value="SC">South Carolina</option>
+                                    <option value="SD">South Dakota</option>
+                                    <option value="TN">Tennessee</option>
+                                    <option value="TX">Texas</option>
+                                    <option value="UT" selected>Utah</option>
+                                    <option value="VT">Vermont</option>
+                                    <option value="VA">Virginia</option>
+                                    <option value="WA">Washington</option>
+                                    <option value="WV">West Virginia</option>
+                                    <option value="WI">Wisconsin</option>
+                                    <option value="WY">Wyoming</option>
+                                    <option value="MX">Mexico</option>
+                                </select>
+                            </div>
+                            <div class="form-group">
+                                <label for="zip">Zip:</label>
+                                <input name="zip" type="text" class="form-control">
+                            </div>
+                            <div class="form-group">
+                                <label for="birthDate">Birth Date:</label>
+                                <input name="birthDate" type="date" class="form-control">
+                            </div>
+                        </div>
+
+                        <div class="container">
+                            <div class="form-group">
+                                <label for="username">Username:</label>
+                                <input name="username" type="text" class="form-control">
+                            </div>
+                            <div class="form-group">
+                                <label for="password">Password:</label>
+                                <input name="password" type="password" class="form-control">
+                            </div>
+                            <div class="form-group">
+                                <label for="sex">Sex:</label><br>
+                                <select name="sex">
+                                    <option value="M">Male</option>
+                                    <option value="F">Female</option>
+                                </select>
+                            </div>
+                            <div class="form-group">
+                                <label for="relationship">Relationship:</label><br>
+                                <select name="relationship">
+                                    <option value="friend">Friend</option>
+                                    <option value="family">Family</option>
+                                    <option value="co-worker">Co-worker</option>
+                                </select>
+                            </div>
+                        </div>
+
+                        <!--buttons-->
+                        <div class="btn-group">
+                            <button type="submit" value="Search" name="action" class="btn btn-default">Search</button>
+                            <button type="submit" value="Update" name="action" class="btn btn-default">Update</button>
+                            <button type="submit" value="Create" name="action" class="btn btn-default">Create</button>
+                        </div>
+                    </form>
+                </div>
+            </footer>
+        </div>
+
+        <!--copyright row-->
+        <div class="container" id="copyright">
+            Copyright &copy; 2017 - All rights reserved - Fred Javalera
+        </div>
+    </body>
+</html>
---
+++ assignment2Return.php
@@ -0,0 +1,193 @@
+<?php
+require_once 'DataBaseConnection.php';
+//$first = htmlentities($_POST['first']);
+//$first = stringtolower($_POST['first']);
+//$first = ucwords($_POST['first']);
+$first = $_POST['first']; //test
+//$last = htmlentities($_POST['last']);
+//$last = stringtolower($_POST['last']);
+//$last = ucwords($_POST['last']);
+$last = $_POST['last'];
+
+//$phone = htmlentities($_POST['phone']);
+$phone = $_POST['phone'];
+
+//$address = htmlentities($_POST['address']);
+$address = $_POST['address'];
+
+//$city = htmlentities($_POST['city']);
+$city = $_POST['city'];
+
+//$state = htmlentities($_POST['state']);
+$state = $_POST['state'];
+
+//$zip = htmlentities($_POST['zip']);
+$zip = $_POST['zip'];
+
+$birthDate = $_POST['birthDate'];
+
+//$username = htmlentities($_POST['username']);
+$username = $_POST['username'];
+
+//$password = htmlentities($_POST['password']);
+$password = $_POST['password'];
+
+//$sex = htmlentities($_POST['sex']);
+$sex = $_POST['sex'];
+
+//$relationship = htmlentities($_POST['relationship']);
+$relationship = $_POST['relationship'];
+
+$action = $_POST['action'];
+?>
+<!DOCTYPE html>
+<html>
+    <head>
+        <!-- Latest compiled JavaScript -->
+        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js">
+        </script>
+        <!-- jQuery library -->
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js">
+        </script>
+        <title>Assignment 2</title>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link href="https://fonts.googleapis.com/css?family=Noto+Sans" rel="stylesheet">
+        <link rel = "stylesheet" type = "text/css" href="css/normalize.css">
+        <link rel="stylesheet" type="text/css" href="css/assignment2Styles.css">
+        <link rel="stylesheet" href="css/bootstrap.min.css">
+        <link rel="stylesheet" href="https://www.w3schools.com/lib/w3.css">
+    </head>
+    <body>
+        <!-- !PAGE CONTENT! -->
+        <div class="w3-content" style="max-width:100%">
+
+            <!-- Header -->
+            <header class="w3-container w3-xlarge w3-padding-18">
+                <a href="/myWebPage.php" class="w3-left w3-btn w3-blue-grey">Home</a>
+
+                <a href="#about" class="w3-right w3-btn w3-blue-grey">About</a>
+                <a href="/exercises.php" class="w3-right w3-btn w3-blue-grey">Exercises</a>
+                <a href="/e-commerce.php" class="w3-right w3-btn w3-blue-grey">E-commerce</a>
+                <a href="/assignment3.php" class="w3-right w3-btn w3-blue-grey">Assignment 3</a>
+                <a href="/assignment2.php" class="w3-right w3-btn w3-blue-grey">Assignment 2</a>
+                <a href="/assignment1.php" class="w3-right w3-btn w3-blue-grey">Assignment 1</a>
+            </header>
+
+            <!-- Photo -->
+            <div class="w3-row">
+                <div class="w3-quarter w3-container"></div>
+                <div class="w3-quarter w3-container">
+                    <img src="/images/friendsAndFamily.png" id="friendsPic" class="img-rounded" alt="Friends and Family">
+                </div>
+                <div class="w3-quarter w3-container"></div>
+                <div class="w3-quarter w3-container"></div>
+            </div>
+        </div>
+
+        <!-- Footer and form -->
+        <div class="container" id="footerContainer">
+            <?php
+            switch ($action)
+            {
+                case "Create":
+                    $insert = "INSERT INTO `Assignment2`.`FriendDB` (`Username`, `First_Name`, `Last_Name`, `Phone_Number`, `Address`, `City`, `State`, `Zip`, `Birthdate`, `Password`, `Sex`, `Relationship`) VALUES ( '$username', '$first', '$last', '$phone', '$address', '$city', '$state', '$zip', '$birthDate', '$password', '$sex', '$relationship')";
+                    $success = $con->query($insert);
+                    if ($success == FALSE)
+                    {
+                        $failmess = "Whole query " . $insert . "<br>";
+                        echo $failmess;
+                        die('Invalid query: ' . mysqli_error($con));
+                    } else
+                    {
+                        echo "Friend or family member was added<br>";
+                        echo "<br><br><a href='assignment2.php'><span id='span'><b>Back</b></span></a>";
+                    }
+                    break;
+
+                case "Update":
+//                            $update = "";
+                    break;
+
+                case "Search":
+                    if ($first != '')
+                    {
+//                            look for matching first name
+                        $search = "SELECT `First_Name`, `Last_Name`, `Phone_Number`, `Username`, `Address`, `City`, `State`, `Zip`, `Birthdate`, `Sex`, `Relationship` FROM `Assignment2` . `FriendDB` WHERE `First_Name` = '$first';";
+                        $return = $con->query($search);
+                        if ($return == FALSE)
+                        {
+                            $message = "No one with the first name" . $first . "was found.<br>";
+                            echo $Message;
+                            die('Invalid query: ' . mysqli_error($con));
+                        } else
+                        {
+                            echo "<table class=\"table\"><th>Username</th><th>First Name</th><th>Last Name</th><th>Phone Number</th><th>Address</th><th>City</th><th>State</th><th>Zip</th><th>Birth Date</th><th>Sex</th><th>Relationship</th>";
+                            while ($row = $return->fetch_assoc())
+                            {
+                                echo "<tr><td>" . $row['Username']
+                                . "</td><td>" . $row['First_Name']
+                                . "</td><td>" . $row['Last_Name']
+                                . "</td><td>" . $row['Phone_Number']
+                                . "</td><td>" . $row['Address']
+                                . "</td><td>" . $row['City']
+                                . "</td><td>" . $row['State']
+                                . "</td><td>" . $row['Zip']
+                                . "</td><td>" . $row['Birthdate']
+                                . "</td><td>" . $row['Sex']
+                                . "</td><td>" . $row['Relationship']
+                                . "</td></tr>";
+                            }
+                            echo "</tbody></table>";
+                        }
+                    }
+                    if ($last != '')
+                    {
+//                            look for matching last name
+                        $search2 = "SELECT `First_Name`, `Last_Name`, `Phone_Number`, `Username`, `Address`, `City`, `State`, `Zip`, `Birthdate`, `Sex`, `Relationship` FROM `Assignment2` . `FriendDB` WHERE `Last_Name` = '$last';";
+                        $return2 = $con->query($search2);
+                        if ($return2 == FALSE)
+                        {
+                            $message2 = "No one with the last name" . $last . "was found.<br>";
+                            echo $Message2;
+                            die('Invalid query: ' . mysqli_error($con));
+                        } else
+                        {
+                            echo "<table class=\"table\"><th>Username</th><th>First Name</th><th>Last Name</th><th>Phone Number</th><th>Address</th><th>City</th><th>State</th><th>Zip</th><th>Birth Date</th><th>Sex</th><th>Relationship</th>";
+                            while ($row2 = $return2->fetch_assoc())
+                            {
+                                echo "<tr><td>" . $row2['Username']
+                                . "</td><td>" . $row2['First_Name']
+                                . "</td><td>" . $row2['Last_Name']
+                                . "</td><td>" . $row2['Phone_Number']
+                                . "</td><td>" . $row2['Address']
+                                . "</td><td>" . $row2['City']
+                                . "</td><td>" . $row2['State']
+                                . "</td><td>" . $row2['Zip']
+                                . "</td><td>" . $row2['Birthdate']
+                                . "</td><td>" . $row2['Sex']
+                                . "</td><td>" . $row2['Relationship']
+                                . "</td></tr>";
+                            }
+                            echo "</tbody></table>";
+                        }
+                    }
+                    break;
+
+                default:
+                    echo "An unexpected error occurred. Pleases try again.";
+                    break;
+            }
+            mysqli_close($con);
+            echo "<a href='assignment2.php'><span id='span'><b>Back</b></span></a>";
+            ?>
+        </div>
+
+        <footer>
+            <div class="container" id="copyright">
+                Copyright &copy; 2017 - All rights reserved - Fred Javalera
+            </div>
+        </footer>
+    </body>
+</html>
