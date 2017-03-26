e9a640eedba4069a63e848f66e2abba7e0c21920
fjavaler <conscientiadivinus@gmail.com>
2017-03-26 17:06:08 -0600

    commit

--- assignment2Return.php
+++ assignment2Return.php
@@ -45,11 +45,11 @@ $action = $_POST['action'];
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <link href="https://fonts.googleapis.com/css?family=Noto+Sans" rel="stylesheet">
+        <link href="https://fonts.googleapis.com/css?family=Gloria+Hallelujah" rel="stylesheet">
         <link rel = "stylesheet" type = "text/css" href="css/normalize.css">
         <link rel="stylesheet" type="text/css" href="css/assignment2Styles.css">
         <link rel="stylesheet" href="css/bootstrap.min.css">
         <link rel="stylesheet" href="https://www.w3schools.com/lib/w3.css">
-        <link href="https://fonts.googleapis.com/css?family=Gloria+Hallelujah" rel="stylesheet">
     </head>
     <body>
         <!-- !PAGE CONTENT! -->
