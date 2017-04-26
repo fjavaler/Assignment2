ab4a9d84e9d9e7368d6ff310562f92e8bca08e0e
fjavaler <conscientiadivinus@gmail.com>
2017-04-25 20:28:42 -0600

    commit

---
+++ eCommerce.php
@@ -0,0 +1,53 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <script src="js/jquery-3.1.1.min.js" type="text/javascript"></script>
+        <script src="js/bootstrap.min.js" type="text/javascript"></script>
+        <title>Assignment 2</title>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link href="https://fonts.googleapis.com/css?family=Noto+Sans" rel="stylesheet">
+        <link href="css/normalize.css" rel = "stylesheet" type = "text/css">
+        <link rel="stylesheet" type="text/css" href="css/assignment2Styles.css">
+        <link rel="stylesheet" href="css/bootstrap.min.css">
+        <link rel="stylesheet" href="https://www.w3schools.com/lib/w3.css">
+        <link rel="shortcut icon" href="images/favicon.ico"/>
+    </head>
+    <body>
+        <!-- !PAGE CONTENT! -->
+        <div class="container-fluid" id="pageContent">
+            <!-- Header -->
+            <nav class="navbar navbar-default navbar-collapse" id="navbar">
+                <div class='navbar-header'>
+                    <div class='btn-group btn-group-justified' role='group' id="navButtons">
+                        <a href="/myWebPage.php" role="button" class="btn btn-lg btn-secondary" id="navButton">Home</a>
+                        <a href="#about" role="button" class="btn btn-lg btn-secondary" id="navButton">About</a>
+                        <a href="/assignment1.php" role="button" class="btn btn-lg btn-secondary" id="navButton">Assignment 1</a>
+                        <a href="/assignment2.php" role="button" class="btn btn-lg btn-secondary" id="navButton">Assignment 2</a>
+                        <a href="/assignment3.php" role="button" class="btn btn-lg btn-secondary" id="navButton">Assignment 3</a>
+                        <a href="/e-commerce.php" role="button" class="btn btn-lg btn-secondary" id="navButton">E-commerce</a>
+                        <a href="/exercises.php" role="button" class="btn btn-lg btn-secondary" id="navButton">Exercises</a>
+                    </div>
+                </div>
+            </nav>
+
+            <!-- Login Prompt -->
+            <div class="container-fluid" id="imgContainer">
+                <div class="row container-fluid">
+                    <div class="span4"></div>
+                    <div class="span4"></div>
+                    <div class="span4"></div>
+                </div>
+            </div>
+        </div>
+        <div class="container" id="border">
+            <img id="border-image" src="images/MosaicBorder.png" alt="Border">
+        </div>
+
+        <!--copyright row-->
+        <div class="container" id="copyright">
+            Copyright &copy; 2017 - All rights reserved - Frederick Javalera
+        </div>
+    </body>
+</html>
