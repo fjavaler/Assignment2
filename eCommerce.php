381dd4edc9422eea2d6cdfb6af0e5152749c66c2
fjavaler <conscientiadivinus@gmail.com>
2017-04-25 22:39:07 -0600

    commit

--- eCommerce.php
+++ eCommerce.php
@@ -34,9 +34,9 @@
             <!-- Login Prompt -->
             <div class="container-fluid" id="imgContainer">
                 <div class="row">
-                    <div class="col-md-6 col-md-offset-3">
-                        <div class="panel panel-login">
-                            <div class="panel-heading">
+                    <div id="loginPanelContainer" class="col-md-6 col-md-offset-3">
+                        <div class="panel panel-login" id="loginPanel">
+                            <div class="panel-heading" id="panelHeading">
                                 <div class="row">
                                     <div class="col-xs-6">
                                         <a href="#" class="active" id="login-form-link">Login</a>
@@ -51,26 +51,26 @@
                                 <div class="row">
                                     <div class="col-lg-12">
                                         <form id="login-form" action="http://phpoll.com/login/process" method="post" role="form" style="display: block;">
-                                            <div class="form-group">
+                                            <div class="form-group col-sm-12">
                                                 <input type="text" name="username" id="username" tabindex="1" class="form-control" placeholder="Username" value="">
                                             </div>
-                                            <div class="form-group">
+                                            <div class="form-group col-sm-12">
                                                 <input type="password" name="password" id="password" tabindex="2" class="form-control" placeholder="Password">
                                             </div>
-                                            <div class="form-group text-center">
+                                            <div class="form-group text-center col-sm-12">
                                                 <input type="checkbox" tabindex="3" class="" name="remember" id="remember">
                                                 <label for="remember"> Remember Me</label>
                                             </div>
-                                            <div class="form-group">
+                                            <div class="form-group col-sm-12">
                                                 <div class="row">
                                                     <div class="col-sm-6 col-sm-offset-3">
                                                         <input type="submit" name="login-submit" id="login-submit" tabindex="4" class="form-control btn btn-login" value="Log In">
                                                     </div>
                                                 </div>
                                             </div>
-                                            <div class="form-group">
+                                            <div class="form-group col-sm-12">
                                                 <div class="row">
-                                                    <div class="col-lg-12">
+                                                    <div class="col-sm-12">
                                                         <div class="text-center">
                                                             <a href="http://phpoll.com/recover" tabindex="5" class="forgot-password">Forgot Password?</a>
                                                         </div>
@@ -109,6 +109,9 @@
         </div>
         <div class="container" id="border">
             <img id="border-image" src="images/MosaicBorder.png" alt="Border">
+        </div>
+        <h1 id="eCommerceH1"><span id="eCommerceText">Login</span></h1>
+        <div class="container" id="spacer">
         </div>
 
         <!--copyright row-->
